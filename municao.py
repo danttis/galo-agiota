24d239f6413c175d292a9000d542724bd6792af5
overhual <mitcrushing@gmail.com>
2021-03-26 20:10:29 -0300

    atualização da posição das balas

--- municao.py
+++ municao.py
@@ -7,7 +7,7 @@ class Municao(pygame.sprite.Sprite):
 
         self.image = pygame.image.load("data/bala.png")
         self.image = pygame.transform.scale(self.image, [15, 6])
-        self.rect = self.image.get_rect()
+        self.rect=pygame.Rect(124,263,-165,-75)
 
         #Velocidade na qual sai da nave e atravessa a tela
         self.speed = 4
